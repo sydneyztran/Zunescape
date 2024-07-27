5acb7330ccf7924b1b3a8e4e393fb5e5e5634d48
sydneyztran <111930717+sydneyztran@users.noreply.github.com>
2024-07-27 12:51:32 -0500

    Delete src/com/zunescape directory

--- src/com/zunescape/player/Player.java
+++
@@ -1,7 +0,0 @@
-package com.zunescape.player;
-
-public interface Player {
-    String getPUID(); //returns player's unique ID
-    String getUsername(); //returns the player's preferred username
-
-}
